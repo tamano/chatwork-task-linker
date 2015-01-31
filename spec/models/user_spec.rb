06bdab1803e0e66e18891d0f201e84f9bc23b128
Yuya TAMANO <everfree.main@gmail.com>
2015-02-01 01:29:01 +0900

    remove usage of depricated function.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,25 +6,25 @@ RSpec.describe User, type: :model do
   describe 'expired?' do
     before :each do
       time_now = Time.local(2015, 1, 1, 0, 0, 0)
-      Time.stub(:now).and_return(time_now)
+      allow(Time).to receive(:now).and_return(time_now)
     end
 
     it 'retrun false if expired_at is past' do
       user = User.new
       user.expires_at = Time.local(2014, 12, 31, 23, 59, 59)
-      expect(user.expired?).to be_true
+      expect(user.expired?).to be_truthy
     end
 
     it 'retrun true if expired_at is just now' do
       user = User.new
       user.expires_at = Time.local(2015, 1, 1, 0, 0, 0)
-      expect(user.expired?).to be_true
+      expect(user.expired?).to be_truthy
     end
 
     it 'retrun true if expired_at is future' do
       user = User.new
       user.expires_at = Time.local(2015, 1, 1, 0, 0, 1)
-      expect(user.expired?).to be_true
+      expect(user.expired?).to be_truthy
     end
   end
 
