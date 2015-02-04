3824e471f8bd12fce21e2b0c1bce750553c88dda
Yuya TAMANO <everfree.main@gmail.com>
2015-02-05 01:05:08 +0900

    Remove all routes from config/routes.rb
    Until I implement views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
 
-  root 'home#index'
+  # root 'home#index'
 
-  devise_for :users, only: [:omniauth_callback, :session], controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-  get '/users/auth/failure' => 'home#index'
+  # devise_for :users, only: [:omniauth_callback, :session], controllers: {
+  #   omniauth_callbacks: 'users/omniauth_callbacks'
+  # }
+  # get '/users/auth/failure' => 'home#index'
 
 end
