95c34f71d1b0351d2c73eec890d15dc3d2230d4d
Yuya TAMANO <everfree.main@gmail.com>
2015-02-06 15:59:48 +0900

    Remove HomeController test temporary.
    Because of the comment-out of routes.rb

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe 'GET index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+    pending 'returns http success'
+    # it 'returns http success' do
+    #   get :index
+    #   expect(response).to have_http_status(:success)
+    # end
   end
 end
