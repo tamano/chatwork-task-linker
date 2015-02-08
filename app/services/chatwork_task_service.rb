e5819c2fa9a3ad31620f5b8f43317d4e7a139a4b
Yuya TAMANO <everfree.main@gmail.com>
2015-02-08 23:59:28 +0900

    Implement ChatworkTaskService class.

--- app/services/chatwork_task_service.rb
+++ app/services/chatwork_task_service.rb
@@ -9,23 +9,26 @@ class ChatworkTaskService
   def fetch_chatwork_tasks
     tasks = []
     fetch_rooms.each do |room|
-      tasks << fetch_chatwork_tasks_in_room(room['id'])
+      tasks << fetch_chatwork_tasks_in_room(room['room_id'])
     end
 
-    tasks
+    # empty elements will removed here.
+    tasks.flatten
   end
 
   def fetch_my_chatwork_tasks
-    fetch_chatwork_tasks.delete_if { |item| item.owner != user.name }
+    fetch_chatwork_tasks.delete_if { |item| item.name != user.name }
   end
 
   def fetch_chatwork_tasks_in_room(room_id)
-    # access to chatwork api and get tasks in a room.
-    room_id
+    ChatWork.api_key = @user.setting.chatwork_token
+    tasks = ChatWork::Task.get(room_id: room_id)
+
+    tasks.instance_of?(ChatWork::APIError) ? [] : tasks
   end
 
   def fetch_rooms
-    # access to chatwork api and get rooms this user joins.
-    nil
+    ChatWork.api_key = @user.setting.chatwork_token
+    ChatWork::Room.get
   end
 end
