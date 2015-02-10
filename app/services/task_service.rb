f76c3b5090f15a2c4c377324e6fdc8ec6e403bca
Yuya TAMANO <everfree.main@gmail.com>
2015-02-10 09:38:24 +0900

    Implement TaskService to send notify message to chatwork.

--- app/services/task_service.rb
+++ app/services/task_service.rb
@@ -5,21 +5,16 @@ class TaskService
     tasks = sysbot_task.fetch_tasks
 
     notify_before.each do |day_before|
-      target_day =
-        if day_before == 'over'
-          'over'
-        else
-          Date.today + day_before
-        end
+      target_day = get_target_day(day_before)
 
       target_tasks = check_limit(tasks, target_day)
 
       target_tasks.each do |task|
         message =
           if target_day == 'over'
-            'over'
+            'over/' + task['body']
           else
-            'day'
+            'day:' + target_day.to_s + '/' + task['body']
           end
 
         send_notice_to_owner(task, message)
@@ -30,16 +25,26 @@ class TaskService
   def check_limit(tasks, target_day = 'over')
     result = []
     tasks.each do |task|
-      if day_before == 'over'
-        result << task if Time.at(task.limit_time).to_date > Day.today
+      if target_day == 'over'
+        result << task if Time.at(task['limit_time']).to_date < Date.today
       else
-        result << task if Time.at(task.limit_time).to_date == target_day
+        result << task if Time.at(task['limit_time']).to_date == target_day
       end
     end
+    result
   end
 
   def send_notice_to_owner(task, message)
     sysbot_message = ChatworkMessageService.new(User.find(1))
-    sysbot_message.send_personal_message(task.name, message)
+    sysbot_message.send_personal_message(task['account']['name'], message)
+    # TODO: using task['name'] as a target identifier is not good. should use member of chatroom.
+  end
+
+  def get_target_day(day_before)
+    if day_before == 'over'
+      'over'
+    else
+      Date.today + day_before
+    end
   end
 end
