ec58e3971f526cf46376049b761edee7a0856b8a
Yuya TAMANO <everfree.main@gmail.com>
2015-03-10 15:17:01 +0900

    Remove Devise related code from app and config

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'expired?' do
-    before :each do
-      time_now = Time.local(2015, 1, 1, 0, 0, 0)
-      allow(Time).to receive(:now).and_return(time_now)
-    end
-
-    it 'retrun true if expired_at is past' do
-      user = User.new
-      user.expires_at = Time.local(2014, 12, 31, 23, 59, 59)
-      expect(user.expired?).to be_truthy
-    end
-
-    it 'retrun false if expired_at is just now' do
-      user = User.new
-      user.expires_at = Time.local(2015, 1, 1, 0, 0, 0)
-      expect(user.expired?).to be_falsey
-    end
-
-    it 'retrun false if expired_at is future' do
-      user = User.new
-      user.expires_at = Time.local(2015, 1, 1, 0, 0, 1)
-      expect(user.expired?).to be_falsey
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
