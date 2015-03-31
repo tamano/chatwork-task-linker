28113d29df1971b9aac0a297ed2663e26620c08f
Yuya TAMANO <everfree.main@gmail.com>
2015-03-31 23:24:11 +0900

    Comment out pending test's cond.

--- spec/models/notify_rule_weekly_spec.rb
+++ spec/models/notify_rule_weekly_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe NotifyRuleWeekly, type: :model do
   describe '#next' do
-    rule = FactoryGirl.create(:notify_rule_weekly)
+    # rule = FactoryGirl.create(:notify_rule_weekly)
     pending
   end
 end
