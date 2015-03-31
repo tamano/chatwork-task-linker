391df0a280897d37e086b707cfe82a6e773d45a8
Yuya TAMANO <everfree.main@gmail.com>
2015-03-31 23:34:16 +0900

    Comment out unused methods temporary.
    To normalize CircleCI test.

--- app/models/notify_rule_daily.rb
+++ app/models/notify_rule_daily.rb
@@ -1,14 +1,14 @@
 # NotifyRuleDaily
 class NotifyRuleDaily < NotifyRule
-  # def next
-  #   current_time = Time.now
+  def next
+    current_time = Time.now
 
-  #   return executing_at if executing_at && executing_at > current_time
+    return executing_at if executing_at && executing_at > current_time
 
-  #   candidate_executing_at = Time.new(
-  #     current_time.year, current_time.month, current_time.day, target_time.hour, target_time.min)
+    candidate_executing_at = Time.new(
+      current_time.year, current_time.month, current_time.day, target_time.hour, target_time.min)
 
-  #   candidate_executing_at += (60 * 60 * 24) while current_time >= candidate_executing_at
-  #   candidate_executing_at
-  # end
+    candidate_executing_at += (60 * 60 * 24) while current_time >= candidate_executing_at
+    candidate_executing_at
+  end
 end
