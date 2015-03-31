e548b04668d80e030a2eef9aba53729dbc1c2c2d
Yuya TAMANO <everfree.main@gmail.com>
2015-03-31 09:44:47 +0900

    test commit for SideCI

--- spec/models/notify_rule_weekly_spec.rb
+++ spec/models/notify_rule_weekly_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe NotifyRuleWeekly, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#next' do
+    rule = FactoryGirl.create(:notify_rule_weekly)
+    pending
+  end
 end
