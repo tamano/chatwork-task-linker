42799d65db4c03c52937206460632913365fc025
Yuya TAMANO <everfree.main@gmail.com>
2015-04-03 15:26:41 +0900

    Fix NotifyRuleDaily factories.

--- spec/factories/notify_rule_dailies.rb
+++ spec/factories/notify_rule_dailies.rb
@@ -2,9 +2,6 @@ FactoryGirl.define do
   factory :notify_rule_daily do
     target_room_id 1
     target_member_id 1
-    target_time '2015-03-15 00:39:07'
     message 'MyString'
-    target_day 1
-    target_date 1
   end
 end
